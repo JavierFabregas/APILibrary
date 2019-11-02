2300e83876c40ceadabd180b648a4c6431cb3796
javierFab <javier_fabregas_apps1ma1819@cev.com>
2019-11-02 12:46:23 +0100

    created model book

---
+++ app/book.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use vendor\laravel\framework\src\Illuminate\Database\Eloquent\Model;
+
+class book extends Model
+{
+    //
+    protected $table = 'books';
+
+   // $books = book::all();
+   // dd($books);
+}
+	
