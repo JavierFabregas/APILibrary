c2477db813c9774687ea8e11aa8fb48fe7e4d180
Fabregas <javier_fabregas_apps1ma1819@cev.com>
2019-11-14 13:35:06 +0100

    Fixed Integrar

--- app/book.php
+++ app/book.php
@@ -8,9 +8,5 @@ class Book extends Model
 {
     //
 	protected $table = 'books';
-<<<<<<< HEAD
-    protected $fillable = ['id','title','description'];
-=======
     protected $fillable = ['title','description'];
->>>>>>> 03476387fd0d7773625fb60f8d51d748165b1ace
 }
